1fc1c3f95ebe6b2628bf0e489d02d3cdbb3294c2
revolyne <revolyne.mwangi@student.moringaschool.com>
2024-03-23 21:07:31 +0300

    cench

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  // This will throw an error because you can't change the value of a constant
+  leastFavoriteCustomer = 'someone else';
+}
+
+// Test the functions
+console.log(upperCaseCustomerName()); // Outputs: "BOB"
